82ac441a979c40384118981d0975369abbe615d5
Ryan <zelda0169@gmailcom>
2020-10-02 21:30:35 -0600

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additonalfeatures
+{
+    public class Person
+    {
+        public Person(string name) : this(name, 25)
+        {
+
+        }
+        
+        public Person FullName (string name, int old)
+        {
+            
+            FirstName = name;
+            Age = old;
+
+         }
+       
+           public string FirstName { get; set; }
+           public string LastName { get; set; }
+           public int Age { get; set; }
+            
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additonalfeatures
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Ryan";
+            Console.WriteLine("Hello my name is {0}", myName);
+            Console.WriteLine("Please enter a number");
+            var x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x * 15);
+
+            Person pers = new Person("Ryan");
+            Console.WriteLine(pers);
+            Console.Read();
+        }
+    }
+}
